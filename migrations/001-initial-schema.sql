8e88ec6c5e809ebe6934df5c01ba7050907b4935
JansenMangulabnan <jan.mangu.fernan@gmail.com>
2025-03-12 14:57:07 +0800

    user table

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -3,10 +3,20 @@ CREATE TABLE Product (
     product_name VARCHAR(255) NOT NULL,
     product_price DECIMAL(10, 2) NOT NULL,
     shop_id INT NOT NULL,
-    FOREIGN KEY (shop_id) REFERENCES Shop(shop_id)
+    edit_permision INT NOT NULL,
+    FOREIGN KEY (shop_id) REFERENCES Shop(shop_id),
+    FOREIGN KEY (edit_permision) REFERENCES User(user_permission)
 );
 
 CREATE TABLE Shop (
     shop_id INT PRIMARY KEY,
     shop_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE User (
+    user_id INT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL
+    user_email VARCHAR(255) NOT NULL
+    user_password VARCHAR(255) NOT NULL
+    user_permission INT NOT NULL
 );
