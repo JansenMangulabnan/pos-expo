78c356c781f1ac285a52bd86efd4eb6f27426d6d
JansenMangulabnan <jan.mangu.fernan@gmail.com>
2025-04-21 02:18:52 +0800

    renamed register to signup

--- index.js
+++ index.js
@@ -76,14 +76,14 @@ app.use(session({
     saveUninitialized: true
 }));
 
-//register
-app.get('/register', (req, res) => {
-    res.render('register', {
-        title: 'Register',
+//register hbs render
+app.get('/signup', (req, res) => {
+    res.render('signup', {
+        title: 'Sign Up',
         styles: [
             'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
             'css/BASE.css',
-            'css/register.css',
+            'css/signup.css',
         ],
         beforeBody: [],
         afterbody: [],
@@ -93,7 +93,7 @@ app.get('/register', (req, res) => {
         ],
         scripts: [
             'https://code.jquery.com/jquery-3.6.0.min.js',
-            'js/register.js',
+            'js/signup.js',
         ]
     });
 });
