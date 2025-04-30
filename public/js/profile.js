56cf79d1af9f83d097a61ce3e99c8c8cd6e3678e
JansenMangulabnan <jan.mangu.fernan@gmail.com>
2025-04-30 14:51:54 +0800

    logout button working

--- public/js/profile.js
+++ public/js/profile.js
@@ -34,5 +34,20 @@ $(document).ready(function () {
             togglePfp = false;
         }
     });
-    
+
+    $(".detail-logout").on("click", function () {
+        $.ajax({
+            url: "/logout",
+            method: "POST",
+            success: function (response) {
+                window.location.href = "/";
+            },
+            error: function (xhr) {
+                const errorMessage =
+                    xhr.responseJSON?.message ||
+                    "An unexpected error occurred.";
+                showPopup(errorMessage);
+            },
+        });
+    });
 });
