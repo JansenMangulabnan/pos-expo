4f8a37125ebef20d8ed96152b8a00fc8706e9bb3
JansenMangulabnan <jan.mangu.fernan@gmail.com>
2025-05-06 02:22:44 +0800

    order display better

--- index.js
+++ index.js
@@ -347,12 +347,27 @@ app.get('/shop', isSeller, async (req, res) => {
             .request()
             .input('shopId', sql.Int, shopId)
             .query(`
-                SELECT *
-                FROM [Order]
-                WHERE order_seller_id = @shopId
+                SELECT 
+                    o.order_id,
+                    p.product_name,
+                    p.product_price,
+                    s.seller_name,
+                    u.user_name,
+                    o.order_date,
+                    o.order_quantity
+                FROM [Order] o
+                LEFT JOIN [Product] p ON o.order_product_id = p.product_id
+                LEFT JOIN [Seller] s ON o.order_seller_id = s.seller_id
+                LEFT JOIN [User] u ON o.order_user_id = u.user_id
+                WHERE o.order_seller_id = @shopId
             `);
 
-        const orders = ordersRecord.recordset;
+        const orders = ordersRecord.recordset.map((order) => {
+            return {
+                ...order,
+                order_price: order.product_price * order.order_quantity,
+            };
+        });
 
         res.render('shop', {
             title: 'Shop',
